fa27d41f6ae5879ce313d31c3af744241aee140d
Deimos <deimos@tildes.net>
2019-02-19 13:04:09 -0700

    Add a generic error page for all HTTP errors
    Previously the site would give an unstyled web-server error on a lot of
    errors (such as trying to visit the page for a username that doesn't
    exist). This just adds a generic error page, but we'll want to add some
    more specialized ones for certain errors.

--- tildes/tildes/views/exceptions.py
+++ tildes/tildes/views/exceptions.py
@@ -3,19 +3,16 @@
 
 """Views used by Pyramid when an exception is raised."""
 
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPException, HTTPForbidden, HTTPNotFound
 from pyramid.request import Request
-from pyramid.view import forbidden_view_config, exception_view_config
+from pyramid.view import (
+    exception_view_config,
+    forbidden_view_config,
+    notfound_view_config,
+)
 from sqlalchemy import cast, desc, func, Text
 
 from tildes.models.group import Group
-
-
-@forbidden_view_config(xhr=False, renderer="error_403.jinja2")
-def forbidden(request: Request) -> dict:
-    """403 Forbidden page."""
-    request.response.status_int = 403
-    return {}
 
 
 @exception_view_config(
@@ -34,3 +31,20 @@ def group_not_found(request: Request) -> dict:
         .all()
     )
     return {"group_suggestions": group_suggestions, "supplied_name": supplied_name}
+
+
+@notfound_view_config(xhr=False, renderer="error_page.jinja2")
+@forbidden_view_config(xhr=False, renderer="error_page.jinja2")
+@exception_view_config(context=HTTPException, xhr=False, renderer="error_page.jinja2")
+def generic_error_page(request: Request) -> dict:
+    """Display a generic error page for all HTTP exceptions."""
+    request.response.status_int = request.exception.status_int
+
+    error = f"Error {request.exception.status_code} ({request.exception.title})"
+
+    if isinstance(request.exception, HTTPForbidden):
+        description = "You don't have access to this page"
+    else:
+        description = request.exception.explanation
+
+    return {"error": error, "description": description}
