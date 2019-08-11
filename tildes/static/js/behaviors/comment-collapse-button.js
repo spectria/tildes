4f03a0fa5c86b0de4764495f1ae5a99516b9caa3
Deimos <deimos@tildes.net>
2019-08-11 17:51:49 -0600

    Switch +/- on comment collapse buttons with CSS
    This was previously being done a bit manually in the JS and HTML, but
    CSS can handle it like this, which simplifies some things (and also
    takes the +/- out of the DOM, which is probably good in this case).

--- tildes/static/js/behaviors/comment-collapse-button.js
+++ tildes/static/js/behaviors/comment-collapse-button.js
@@ -14,12 +14,6 @@ $.onmount("[data-js-comment-collapse-button]", function() {
             $comment.toggleClass("is-comment-collapsed");
         }
 
-        if ($comment.hasClass("is-comment-collapsed")) {
-            $this.text("+");
-        } else {
-            $this.html("&minus;");
-        }
-
         $this.blur();
     });
 });
