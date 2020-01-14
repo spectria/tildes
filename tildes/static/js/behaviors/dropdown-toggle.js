ed2cdd2c4b0fb4289889eabfcfc4932f19372b1e
Deimos <deimos@tildes.net>
2020-01-13 17:34:01 -0700

    Remove animation on dropdown
    This also removes the attempt to close the dropdown if you the toggle
    again while it's already open, since it seems like that isn't working
    correctly anyway.

--- tildes/static/js/behaviors/dropdown-toggle.js
+++ tildes/static/js/behaviors/dropdown-toggle.js
@@ -6,16 +6,11 @@
 // with the dropdown-toggle class so that this behavior is always applied to dropdowns.
 $.onmount(".dropdown-toggle", function() {
     $(this).click(function() {
-        if ($(this).is(":focus")) {
-            // If the button was already focused (so the menu is visible), close it
-            $(this).blur();
-        } else {
-            // Spectre.css's dropdown menus use the focus event to display the menu,
-            // but Safari and Firefox on OSX don't give focus to a <button> when it's
-            // clicked. More info:
-            // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
-            // This should make the behavior consistent across all browsers
-            $(this).focus();
-        }
+        // Spectre.css's dropdown menus use the focus event to display the menu,
+        // but Safari and Firefox on OSX don't give focus to a <button> when it's
+        // clicked. More info:
+        // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/button#Clicking_and_focus
+        // This should make the behavior consistent across all browsers
+        $(this).focus();
     });
 });
