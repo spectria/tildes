1c739d97364bebf9e42dceae685eb6cf0657ca8c
Deimos <deimos@tildes.net>
2020-03-12 18:25:31 -0600

    Update groups page layout + show activity stats
    This replaces the groups page with a new list style that doesn't include
    the ability to subscribe/unsubscribe directly on the page, and also
    shows approximate activity statistics (daily topics/comments) for each
    group.

--- tildes/tildes/views/group.py
+++ tildes/tildes/views/group.py
@@ -6,6 +6,7 @@
 from pyramid.request import Request
 from pyramid.view import view_config
 
+from tildes.enums import GroupStatType
 from tildes.models.group import Group
 
 
@@ -14,4 +15,32 @@ def get_list_groups(request: Request) -> dict:
     """Show a list of all groups."""
     groups = request.query(Group).order_by(Group.path).all()
 
-    return {"groups": groups}
+    # converting this to SQLAlchemy seems like way more trouble than it's worth
+    posting_stats = request.db_session.execute(
+        """
+        select path, stat, ceil(sum(value) / count(*))
+        from group_stats
+            left join groups using (group_id)
+        where period && tstzrange(now() - interval '7 days', now())
+        group by path, stat
+        order by path, stat;
+    """
+    ).fetchall()
+
+    daily_comment_counts = {
+        row[0]: int(row[2])
+        for row in posting_stats
+        if row[1] == GroupStatType.COMMENTS_POSTED.name
+    }
+
+    daily_topic_counts = {
+        row[0]: int(row[2])
+        for row in posting_stats
+        if row[1] == GroupStatType.TOPICS_POSTED.name
+    }
+
+    return {
+        "groups": groups,
+        "daily_comment_counts": daily_comment_counts,
+        "daily_topic_counts": daily_topic_counts,
+    }
