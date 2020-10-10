f31052670a0ef61814a8e2e2a51c81bba44e6cec
Deimos <deimos@tildes.net>
2020-10-09 22:00:08 -0600

    Remove "via" annotations from requirements files
    I should have just done this all along, these have been way more trouble
    than they're worth.
    
    If the information is needed, it's always possible to just do a temp run
    of pip-compile without --no-annotate or use a dedicated tool like
    pipdeptree.

--- tildes/tasks.py
+++ tildes/tasks.py
@@ -3,7 +3,6 @@
 
 """Contains tasks that can be run through the invoke tool."""
 
-import re
 from pathlib import Path
 
 from invoke import task
@@ -105,25 +104,8 @@ def type_checking(context):
 def update_pip_requirements(context):
     """Use pip-tools to update package versions in the requirements files."""
 
-    def build_and_clean(context, name):
-        """Update a pip requirements file and clean up the result."""
-        in_filename = Path(name).with_suffix(".in")
-        out_filename = Path(name).with_suffix(".txt")
-
-        print(f"Updating package versions from {in_filename}")
-        context.run(f"pip-compile --no-header --quiet --upgrade {in_filename}")
-
-        # Salt's pip module is currently broken if any comments in the requirements
-        # file have "-r" in them, so we need to remove all of those comments
-        with open(out_filename, "r") as req_file:
-            req_lines = req_file.readlines()
-
-        # remove any comments that include an -r reference
-        # (meaning it's a package that was specifically installed, not a dependency)
-        cleaned_lines = [re.sub(r"\s+# via.* -r .*", "", line) for line in req_lines]
-
-        with open(out_filename, "w") as req_file:
-            req_file.writelines(cleaned_lines)
-
-    build_and_clean(context, "requirements")
-    build_and_clean(context, "requirements-dev")
+    for filename in ("requirements.in", "requirements-dev.in"):
+        print(f"Updating package versions from {filename}")
+        context.run(
+            f"pip-compile --no-header --no-annotate --quiet --upgrade {filename}"
+        )
